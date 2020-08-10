0613d5d8e37ffd3940a86ea68a3a1f8e58ba78e6
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-10 11:22:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dizi_Projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("dizi boyutunu giriniz:"); // kullancııdan dizi boyutu alınması
+            int boyut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] dizi = new int[boyut];
+            string[] öğrenci = new string[boyut]; // bir öğrenci dizisinin oluşturulması.
+
+            for (int i = 0; i < boyut; i++)
+            {
+                Console.Write("öğrenci adini giriniz:"); // kullanıcıdan, girilen boyut kadar isim alınması
+                öğrenci[i] = Console.ReadLine();
+            }
+            Console.WriteLine("---------------------------");
+   
+        }
+    }
+}
